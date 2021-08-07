d120b79a0efc1dc426b2ecaf685eaa87cc668e5c
28cyc <by890228@gmail.com>
2021-08-07 20:39:01 +0800

    add project

---
+++ ChatBot/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBot.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public string Test([FromForm] string name)
+        {
+            return $"hello {name}!!!";
+        }
+    }
+}
