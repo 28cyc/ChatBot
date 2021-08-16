5a8577a3b40f3f403e399f24566efa2b857b382f
28cyc <by890228@gmail.com>
2021-08-16 19:18:21 +0800

    fix

---
+++ ChatBot/Dac/OrderDac.cs
@@ -0,0 +1,21 @@
+﻿using ChatBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBot.Dac
+{
+	public class OrderDac: _Dac
+	{
+		/// <summary>
+		/// 取得所有餐點資料
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<FoodModel> getAllFood()
+		{
+			return DB.ExecuteQuery<FoodModel>("SELECT * FROM Food");
+		}
+
+	}
+}
