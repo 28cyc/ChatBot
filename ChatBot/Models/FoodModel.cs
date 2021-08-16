9be73136c4c54d6fdd9f68029fa8024520967ddd
28cyc <by890228@gmail.com>
2021-08-16 18:55:41 +0800

    add food

---
+++ ChatBot/Models/FoodModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBot.Models
+{
+    public class FoodModel
+    {
+        /// <summary>
+        /// 餐點編號
+        /// </summary>
+        public int Food_ID { get; set; }
+
+        /// <summary>
+        /// 餐點名稱
+        /// </summary>
+        public string FoodName { get; set; }
+
+        /// <summary>
+        /// 餐點價格
+        /// </summary>
+        public int FoodPrice { get; set; }
+
+        /// <summary>
+        ///餐點描述
+        /// </summary>
+        public string FoodDescription { get; set; }
+
+        /// <summary>
+        /// 餐點產地
+        /// </summary>
+        public string FoodFrom { get; set; }
+    }
+}
---
+++ ChatBot/Service/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using ChatBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBot.Service
+{
+	public class OrderService
+	{
+		Dac.OrderDac orderDac = new Dac.OrderDac();
+
+		/// <summary>
+		/// 取得所有餐點資料
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<FoodModel> getAllFood()
+		{
+			return orderDac.getAllFood();
+		}
+	}
+}
